a69660dc6a584574fcb2433fa06b9ee928cef48f
Elementary786 <109067271+Elementary786@users.noreply.github.com>
2022-07-11 20:40:06 +1000

    basic game conditions

--- tennis.ts
+++ tennis.ts
@@ -8,49 +8,88 @@ type PlayerState = {
 type PlayerName = string
 
 interface DiUSTennis<P1, P2> {
-	pointWonBy(name: P1 | P2)
+	pointWonBy(name: P1 | P2): void
 	score(): string
 }
+// match.pointWonBy("player 3") should be a type error now
+
+// helper functions for scoring conditions
+const winByTwo = (a: number, b: number) => (a - b) >= 2;
+const atLeastFour = (a: number) => a >= 4;
+const deuce = (a: number, b: number) => a >= 3 && b >= 3 && a === b;
+const advantage = (a: number, b: number) => a >= 3 && b >= 3 && a > b;
+const wonGame = (a: number, b: number) => atLeastFour(a) && winByTwo(a, b);
+const wonSet = (a: number, b: number) => a >= 6 && winByTwo(a, b);
 
 class Match<P1 extends PlayerName, P2 extends PlayerName> implements DiUSTennis<P1, P2> {
 	private player1: PlayerState = { sets: 0, games: 0, points: 0 };
-	private player2: PlayerState = { sets: 0, games: 0, points: 0 }
+	private player2: PlayerState = { sets: 0, games: 0, points: 0 };
 
 	constructor(private readonly player1Name: P1, private readonly player2Name: P2) { }
 
 	pointWonBy(name: P1 | P2) {
+		this.getPlayerState(name).points += 1;
 
+		// I don't **really** like creating references to class properties but will here for brevity
+		const p1 = this.player1.points;
+		const p2 = this.player2.points;
+
+		// check game winning conditions
+		if (wonGame(p1, p2)) {
+			this.player1.games += 1;
+			this.resetScore();
+		} else if (wonGame(p2, p1)) {
+			this.player2.games += 1;
+			this.resetScore();
+		}
+
+		// check set winning conditions
+		if (wonSet(p1, p2)) {
+			this.player1.sets += 1; // congratulations player 1
+		} else if (wonSet(p2, p1)) {
+			this.player2.sets += 1; // congratulations player 2
+		}
 	}
 
 	score(): string {
-		return ''
+		return `${this.player1.games}-${this.player2.games}, ${this.player1.points}-${this.player2.points}`;
+	}
+
+	private resetScore(): void {
+		this.player1.points = 0;
+		this.player2.points = 0;
+	}
+
+	private getPlayerState(name: string) {
+		if (name === this.player1Name) {
+			return this.player1;
+		} else {
+			return this.player2;
+		}
 	}
 }
 
 /* From dius_tennis README */
-const match = new Match("player 1", "player 2");
-match.pointWonBy("player 1");
-match.pointWonBy("player 2");
+const match = new Match('player 1', 'player 2');
+match.pointWonBy('player 1');
+match.pointWonBy('player 2');
 // this will return "0-0, 15-15"
-match.score();
+console.log(match.score()); // I prefer to return strings rather than do the printing inside a method
 
-match.pointWonBy("player 1");
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
+match.pointWonBy('player 1');
 // this will return "0-0, 40-15"
-match.score();
+console.log(match.score());
 
-match.pointWonBy("player 2");
-match.pointWonBy("player 2");
+match.pointWonBy('player 2');
+match.pointWonBy('player 2');
 // this will return "0-0, Deuce"
-match.score();
+console.log(match.score());
 
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
 // this will return "0-0, Advantage player 1"
-match.score();
+console.log(match.score());
 
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
 // this will return "1-0"
-match.score();
-
-  // match.poingWonBy("player 3") should be a type error now
-  // match.pointWonBy("player 3");
+console.log(match.score());
